69dd09c761251b67dca26d813c5770ad9ef0fd4e
Gergely Papi <gergely.papi.cc@gmail.com>
2018-05-23 11:20:07 +0200

    Added hashing utility for passwords

---
+++ src/main/java/com/codecool/miniuserapi/assigment/Utils/PasswordHashed.java
@@ -0,0 +1,19 @@
+package com.codecool.miniuserapi.assigment.Utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordHashed {
+    // Hash a password for the first time
+
+    public String passwordHasher (String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+    }
+
+    public Boolean passwordVerifier (String candidate, String hashed) {
+        if (BCrypt.checkpw(candidate, hashed))
+            return true;
+        else
+            return false;
+    }
+
+}
