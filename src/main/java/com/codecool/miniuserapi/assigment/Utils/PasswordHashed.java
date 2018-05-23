3f3d57be40f07fba90f4eac0f988a6fbd8babf19
Gergely Papi <gergely.papi.cc@gmail.com>
2018-05-23 20:58:57 +0200

    Finalized hash validation function.

--- src/main/java/com/codecool/miniuserapi/assigment/Utils/PasswordHashed.java
+++ src/main/java/com/codecool/miniuserapi/assigment/Utils/PasswordHashed.java
@@ -3,14 +3,12 @@ package com.codecool.miniuserapi.assigment.Utils;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class PasswordHashed {
-    // Hash a password for the first time
-
     public String passwordHasher (String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(12));
     }
 
-    public Boolean passwordVerifier (String candidate, String hashed) {
-        return BCrypt.checkpw(candidate, hashed);
+    public Boolean passwordVerifier (String plainTextPw, String hashedPw) {
+        return BCrypt.checkpw(plainTextPw, hashedPw);
     }
 
 }
