11d6651da665e3df765f27a528e882ce3d117f27
Gergely Papi <gergely.papi.cc@gmail.com>
2018-05-23 09:21:13 +0200

    Project inicialization

---
+++ src/main/java/com/codecool/miniuserapi/assigment/MiniUserApiApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.miniuserapi.assigment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniUserApiApplication.class, args);
+	}
+}
